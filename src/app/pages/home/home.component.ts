5c6427cde6b60dac533ba1b6ea51fb3c6fcece61
Ali Haider <alihaider78222@gmail.com>
2023-11-19 18:49:45 +0500

    image meta tag added

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -37,6 +37,7 @@ export class HomeComponent implements OnInit {
         this.seo.updateMetaTags({
           title :  'Blogs | Angular SSR Boilerplate',
           description :  'List of all blogs',
+          image : '../assets/angular.svg'
         });
         // Load list of blogs
         this.loadBlogsList();
@@ -72,6 +73,7 @@ export class HomeComponent implements OnInit {
       this.seo.updateMetaTags({
         title :  this.blog_detail?.title,
         description :  this.blog_detail?.description,
+        image : this.blog_detail?.photo_url
       });
 
     }).catch( error => {
--- src/app/services/seo/seo.service.ts
+++ src/app/services/seo/seo.service.ts
@@ -4,6 +4,7 @@ import { Meta, Title } from '@angular/platform-browser';
 export interface SEO {
   title?: string;
   description?: string;
+  image?: string;
   url?: string;
 }
 
@@ -22,11 +23,15 @@ export class SeoService {
   // Update Meta Tags
   updateMetaTags(seo : SEO){
     this.ngTitle.setTitle(seo?.title ?? '');
+    this.meta.updateTag({ name: 'description', content:  seo?.title ?? '' });
+
     this.meta.updateTag({ itemprop: 'name', content: seo?.title ?? '' });
     this.meta.updateTag({ itemprop: 'description', content: seo?.description ?? '' });
-    this.meta.updateTag({ name: 'twitter:title', content:  seo?.title ?? '' });
-    this.meta.updateTag({ name: 'twitter:description', content: seo?.description ?? '' });
     this.meta.updateTag({ property: 'og:title', content:  seo?.title ?? '' });
     this.meta.updateTag({ property: 'og:description', content: seo?.description ?? '' });
+    this.meta.updateTag({ property: 'og:image', content: seo?.image ?? '' });
+    this.meta.updateTag({ name: 'twitter:title', content:  seo?.title ?? '' });
+    this.meta.updateTag({ name: 'twitter:description', content: seo?.description ?? '' });
+    this.meta.updateTag({ name: 'twitter:image', content: seo?.image ?? '' });
   }
 }

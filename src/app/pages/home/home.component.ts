ba26604690d7083bbef58ad9b7de0660d3e76957
Ali Haider <alihaider78222@gmail.com>
2023-11-19 21:01:59 +0500

    scss refactoring

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -6,7 +6,7 @@ import { SeoService } from 'src/app/services/seo/seo.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
 
